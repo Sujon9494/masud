514db1ba21e383ee7bc703e92f8409645e3477b6
Sujon9494 <sujonhosen9466@gmail.com>
2018-09-12 10:40:58 +0600

    first commit

---
+++ lok.php
@@ -0,0 +1,21 @@
+<?php
+    $x =5;
+    $y =10;
+
+    if($x <=10)
+    {
+        if($y >=5)
+        {
+            echo "y is gater than";
+        }
+        else
+        {
+            echo "y is less than";
+        }
+        }
+        
+    else
+    {
+        echo "x is getar than";
+    }
+?>
